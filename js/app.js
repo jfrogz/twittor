06854302ae4b6463140cab2b5abc0c79ea8003e7
Robles Guzman Juan Fernando <A85013615@ts-mex.com>
2019-06-27 10:54:15 -0500

    Modificaciones para ios

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var swLocation = '/twittor/sw.js'
 
 if(navigator.serviceWorker){
     if(url.includes('localhost')){
-        sw = '/sw.js';
+        swLocation = '/sw.js';
     }
     navigator.serviceWorker.register(swLocation);
 }
