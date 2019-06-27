d52374d2b225b06b6f0123db67f44703d8e40210
Robles Guzman Juan Fernando <A85013615@ts-mex.com>
2019-06-27 10:28:29 -0500

    Modificamos la ruta del SW

--- sw.js
+++ sw.js
@@ -5,12 +5,16 @@ const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
-const APP_SHELL = ['/', 'index.html', 'css/style.css', 'img/favicon.ico'
-    , 'img/avatars/hulk.jpg'
-    , 'img/avatars/ironman.jpg'
-    , 'img/avatars/spiderman.jpg'
-    , 'img/avatars/thor.jpg'
-    , 'img/avatars/wolverine.jpg', 'js/app.js'];
+const APP_SHELL = [
+    //'/', 
+    'index.html', 
+    'css/style.css', 
+    'img/favicon.ico', 
+    'img/avatars/hulk.jpg', 
+    'img/avatars/ironman.jpg', 
+    'img/avatars/spiderman.jpg', 
+    'img/avatars/thor.jpg', 
+    'img/avatars/wolverine.jpg', 'js/app.js'];
 
 const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
@@ -20,35 +24,35 @@ const APP_SHELL_INMUTABLE = [
     "js/libs/jquery.js"
 ];
 
-self.addEventListener('install', e =>{
+self.addEventListener('install', e => {
     const cacheStatic = caches.open(STATIC_CACHE)
-        .then(cache => cache.addAll( APP_SHELL));
+        .then(cache => cache.addAll(APP_SHELL));
 
     const cacheInmutable = caches.open(INMUTABLE_CACHE)
-        .then(cache => cache.addAll( APP_SHELL_INMUTABLE));
+        .then(cache => cache.addAll(APP_SHELL_INMUTABLE));
 
     e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 });
 
 self.addEventListener('active', e => {
-    const respuesta = caches.keys().then( keys => {
-        keys.forEach( key => {
-            if (  key !== STATIC_CACHE && key.includes('static') ) {
+    const respuesta = caches.keys().then(keys => {
+        keys.forEach(key => {
+            if (key !== STATIC_CACHE && key.includes('static')) {
                 return caches.delete(key);
             }
         });
     });
-    e.waitUntil( respuesta );
+    e.waitUntil(respuesta);
 });
 
-self.addEventListener('fetch', e =>{
+self.addEventListener('fetch', e => {
     const respuesta = caches.match(e.request).then(res => {
-        if(res) {
+        if (res) {
             return res;
-        }else{
-           return fetch(e.request).then(newRes => {
+        } else {
+            return fetch(e.request).then(newRes => {
                 return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
-           });
+            });
         }
     });
 
